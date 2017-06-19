2825bb5a44d7959dd77e5184b13a7832dda0dcff
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-06-19 15:40:11 +0100

    Added header only target for Boost.System, and stubbed out the CMakeLists for Boost.System's dependencies

--- libs/system/build/library.cmake
+++ libs/system/build/library.cmake
@@ -1,6 +1,7 @@
+set(${PROJECT_NAME}_HEADERS)
 set(${PROJECT_NAME}_SOURCES)
 # Bring in the **auto-generated** list of headers from ../../boost/system/*
-# It extends the ${PROJECT_NAME}_SOURCES variable
+# It extends the ${PROJECT_NAME}_HEADERS variable
 include("build/headers.cmake")
 # Bring in the **auto-generated** list of sources from src/*
 # It extends the ${PROJECT_NAME}_SOURCES variable
@@ -10,51 +11,48 @@ include("build/sources.cmake")
 # and build exactly the same lists. But it is slow, slow, slow. So the above
 # is much less impact on development.
 
-add_library(boost_system-sl STATIC ${${PROJECT_NAME}_SOURCES})
+# Create a header only library target
+add_library(boost_system-hl INTERFACE)
+target_sources(boost_system-hl INTERFACE ${${PROJECT_NAME}_HEADERS})
 # This path must be searched for header files during build and by anyone who links/consumes me
-target_include_directories(boost_system-sl PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/../..")
+target_include_directories(boost_system-hl INTERFACE ${${PROJECT_NAME}_HEADERS_PATH})
+# This macro must be defined both during build and by anyone who links/consumes
+# this library
+target_compile_definitions(boost_system-hl INTERFACE BOOST_ERROR_CODE_HEADER_ONLY=1)
+# This target requires C++ 98 during build and in anyone who links/consumes
+# this library
+target_compile_features(boost_system-hl INTERFACE cxx_std_98)
+add_library(boost::system::hl ALIAS boost_system-hl)
+
+# Create a static library target
+add_library(boost_system-sl STATIC ${${PROJECT_NAME}_HEADERS} ${${PROJECT_NAME}_SOURCES})
+# This path must be searched for header files during build and by anyone who links/consumes me
+target_include_directories(boost_system-sl PUBLIC ${${PROJECT_NAME}_HEADERS_PATH})
 # This macro must be defined both during build and by anyone who links/consumes
 # this library
 target_compile_definitions(boost_system-sl PUBLIC BOOST_SYSTEM_STATIC_LINK=1)
 # This target requires C++ 98 during build and in anyone who links/consumes
 # this library
 target_compile_features(boost_system-sl PUBLIC cxx_std_98)
-set_target_properties(boost_system-sl PROPERTIES
-  # Place the library into a "lib" or "bin" directory at the root of the build depending on its kind
-  ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-  LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-  RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-  # If you want DLLs consuming this static library to be linkable
-  POSITION_INDEPENDENT_CODE ON
-  # Name similarly to how Boost.Build names them
-  OUTPUT_NAME "${PROJECT_NAME}-${CMAKE_CXX_COMPILER_ID}-mt-$<CONFIG>-${BOOST_VERSION}-sl"
-)
 add_library(boost::system::sl ALIAS boost_system-sl)
 
-# Create a shared library named similarly to how Boost.Build names them
-add_library(boost_system-dl SHARED ${${PROJECT_NAME}_SOURCES})
+# Create a shared library target
+add_library(boost_system-dl SHARED ${${PROJECT_NAME}_HEADERS} ${${PROJECT_NAME}_SOURCES})
 # This path must be searched for header files during build and by anyone who links/consumes me
-target_include_directories(boost_system-dl PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/../..")
+target_include_directories(boost_system-dl PUBLIC ${${PROJECT_NAME}_HEADERS_PATH})
 # This macro must be defined both during build and by anyone who links/consumes
 # this library
 target_compile_definitions(boost_system-dl PUBLIC BOOST_SYSTEM_DYN_LINK=1)
 # This target requires C++ 98 during build and in anyone who links/consumes
 # this library
 target_compile_features(boost_system-dl PUBLIC cxx_std_98)
-set_target_properties(boost_system-dl PROPERTIES
-  # Place the library into a "lib" or "bin" directory at the root of the build depending on its kind
-  ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-  LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
-  RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
-  # Name similarly to how Boost.Build names them
-  OUTPUT_NAME "${PROJECT_NAME}-${CMAKE_CXX_COMPILER_ID}-mt-$<CONFIG>-${BOOST_VERSION}-dl"
-)
 # Alias this to a more programmer friendly name
 add_library(boost::system::dl ALIAS boost_system-dl)
 
 # Install these targets as follows
-install(TARGETS boost_system-sl boost_system-dl EXPORT boost COMPONENT system
+install(TARGETS boost_system-hl boost_system-sl boost_system-dl EXPORT boost COMPONENT system
         RUNTIME DESTINATION bin
+        PUBLIC_HEADER DESTINATION include
         LIBRARY DESTINATION lib
         ARCHIVE DESTINATION lib
 )
